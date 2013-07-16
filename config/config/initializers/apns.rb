98fe4ef1c412b0de2c355eba5cb0107d7c7fe285
chenlong <KLoong777@163.com>
2013-07-16 13:33:40 +0800

    delete no user file

--- config/config/initializers/apns.rb
+++
@@ -1,8 +0,0 @@
-require 'apns/core'
-require 'apns/notification'
-APNS.host = 'gateway.push.apple.com'
-# gateway.sandbox.push.apple.com is default
-APNS.pem  = '/config/cert.pem'
-# this is the file you just created
-APNS.port = 2195
-# this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
